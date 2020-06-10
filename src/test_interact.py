ea61be2048cd6a6fd5a81fd5c63dbf93e45796ac
Alexander Jensen <ajexsen@gmail.com>
2020-06-10 15:12:25 +0200

    add x32 version

--- src/test_interact.py
+++ src/test_interact.py
@@ -1,7 +1,8 @@
 from mlagents_envs.environment import UnityEnvironment
 # This is a non-blocking call that only loads the environment.
-env = UnityEnvironment(file_name="../tennis/Tennis", seed=1, side_channels=[])
+env = UnityEnvironment(file_name="../tennis/x64/Tennis", seed=1, side_channels=[])
 # Start interacting with the evironment.
+print("1 -")
 env.reset()
-print("done!")
+print("2 - ")
 behavior_names = env.behavior_spec.keys()
