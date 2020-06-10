584c7448501f43ce2b819c31ab27149027214df5
Alexander Jensen <ajexsen@gmail.com>
2020-06-10 14:45:10 +0200

    add build (old version downloaded from internet)

--- src/test_interact.py
+++ src/test_interact.py
@@ -1,6 +1,6 @@
 from mlagents_envs.environment import UnityEnvironment
 # This is a non-blocking call that only loads the environment.
-env = UnityEnvironment(file_name="3DBall", seed=1, side_channels=[])
+env = UnityEnvironment(file_name="../tennis/Tennis", seed=1, side_channels=[])
 # Start interacting with the evironment.
 env.reset()
 behavior_names = env.behavior_spec.keys()
