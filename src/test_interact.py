076e28bfc8d08ef749697d35f9ff98498244a6ce
Alexander Jensen <a.jensen@campus.lmu.de>
2020-06-11 02:53:56 +0200

    interact env functional

--- src/test_interact.py
+++ src/test_interact.py
@@ -1,8 +1,12 @@
 from mlagents_envs.environment import UnityEnvironment
 # This is a non-blocking call that only loads the environment.
-env = UnityEnvironment(file_name="../tennis/x64/Tennis", seed=1, side_channels=[])
+env = UnityEnvironment(file_name=None)
 # Start interacting with the evironment.
 print("1 -")
 env.reset()
 print("2 - ")
-behavior_names = env.behavior_spec.keys()
+# for i in range(100):
+#     env.step()
+behavior_names = env.get_behavior_names()
+behavior_spec0 = env.get_behavior_spec(behavior_names[0])
+print(behavior_spec0)
