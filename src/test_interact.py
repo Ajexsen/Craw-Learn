8d6f34e273fdb8d38e3f34a8611e53186743c941
Alexander_Rose <alexander.rose@q-perior.com>
2020-06-11 13:32:54 +0200

    crawler

--- src/test_interact.py
+++ src/test_interact.py
@@ -1,6 +1,14 @@
 from mlagents_envs.environment import UnityEnvironment
+from gym_unity.envs import UnityToGymWrapper
+from stable_baselines3.ppo import PPO
+from stable_baselines3.ppo.policies import MlpPolicy
 # This is a non-blocking call that only loads the environment.
-env = UnityEnvironment(file_name="../tennis/Tennis", seed=1, side_channels=[])
+unity_env = UnityEnvironment(file_name="../crawler_single/UnityEnvironment", seed=1, side_channels=[])
+env = UnityToGymWrapper(unity_env=unity_env)
+model = PPO(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=1000)
+print(str(env.action_space))
+print(str(env.observation_space))
+print(str(env.name))
+print(str(env))
 # Start interacting with the evironment.
-env.reset()
-behavior_names = env.behavior_spec.keys()
