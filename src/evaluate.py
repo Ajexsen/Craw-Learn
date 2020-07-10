20d92bdf24734bc731f3c51fd71706a6b70cef61
Alexander Jensen <a.jensen@campus.lmu.de>
2020-07-10 17:48:28 +0200

    modify eval function

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,7 +2,9 @@ import numpy as np
 # import gym
 import torch
 
-
+import ppo2 as a
+from gym_unity.envs import UnityToGymWrapper
+from mlagents_envs.environment import UnityEnvironment
 
 def evaluate_policy(model, env, n_eval_episodes=10, deterministic=True,
                     render=False, callback=None, reward_threshold=None,
@@ -44,14 +46,17 @@ def evaluate_policy(model, env, n_eval_episodes=10, deterministic=True,
             action = action_dist.sample().cpu()
             # action, _, _ = model.policy(obs)
             obs, reward, done, _info = env.step(torch.flatten(action).numpy())
+            # print(reward)
             episode_reward += reward
             if callback is not None:
                 callback(locals(), globals())
             episode_length += 1
             if render:
                 env.render()
-        episode_rewards.append(episode_reward)
-        episode_lengths.append(episode_length)
+    #     print("--------------------")
+    #     episode_rewards.append(episode_reward)
+    #     episode_lengths.append(episode_length)
+    # print(episode_rewards)
     mean_reward = np.mean(episode_rewards)
     std_reward = np.std(episode_rewards)
     if reward_threshold is not None:
@@ -60,3 +65,44 @@ def evaluate_policy(model, env, n_eval_episodes=10, deterministic=True,
     if return_episode_rewards:
         return episode_rewards, episode_lengths
     return mean_reward, std_reward
+
+if __name__ == '__main__':
+    # window_path = "../crawler_single/UnityEnvironment"
+    linux_path = "../crawler_single/linux/dynamic_server/crawler_dynamic.x86_64"
+    unity_env = UnityEnvironment(file_name=linux_path, seed=1, side_channels=[])
+    env = UnityToGymWrapper(unity_env=unity_env)
+
+    params = {}
+
+    params["alpha"] = 3e-4
+    params["tau"] = 0.95
+    # not tune (default)
+    params["clip"] = 0.2
+
+    # (Alex) 1024 nodes, 32 mini batch size
+    # hidden_units: 2^x, bigger as input [256, 512]
+    params["hidden_units"] = 512
+
+    params["nr_output_features"] = env.action_space.shape[0]
+    params["nr_input_features"] = env.observation_space.shape[0]
+    params["env"] = env
+
+    # Hyperparameters
+    # min. two layer
+    training_episodes = 5000
+
+    params["update_episodes"] = 15
+    params["ppo_epochs"] = 5
+    params["beta"] = 0.05
+    params["gamma"] = 0.99
+
+    print("update_episodes:", params["update_episodes"], ", ppo_epochs:", params["ppo_epochs"], ", beta: ",
+          params["beta"],
+          ", gamma", params["gamma"])
+
+    agent = a.PPOLearner(params, None)
+
+    for i in range(160, 175):
+        agent.ppo_net = torch.load("PPONet_{}".format(i))
+        mean_reward, std_reward = evaluate_policy(agent.ppo_net, env, n_eval_episodes=10, render=True)
+        print("{}: {}, {}".format(i, mean_reward, std_reward))
