2102c4630b232497eb4af930cecea79a6ba90ac4
Alexander Jensen <a.jensen@campus.lmu.de>
2020-07-10 17:54:39 +0200

    fix eval function

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,71 +6,50 @@ import ppo2 as a
 from gym_unity.envs import UnityToGymWrapper
 from mlagents_envs.environment import UnityEnvironment
 
-def evaluate_policy(model, env, n_eval_episodes=10, deterministic=True,
-                    render=False, callback=None, reward_threshold=None,
-                    return_episode_rewards=False):
-    """
-    source: https://stable-baselines3.readthedocs.io/en/master/_modules/stable_baselines3/common/evaluation.html?highlight=evaluate_policy
 
-    Runs policy for ``n_eval_episodes`` episodes and returns average reward.
-    This is made to work only with one env.
-
-    :param model: (BaseAlgorithm) The RL agent you want to evaluate.
-    :param env: (gym.Env or VecEnv) The gym environment. In the case of a ``VecEnv``
-        this must contain only one environment.
-    :param n_eval_episodes: (int) Number of episode to evaluate the agent
-    :param deterministic: (bool) Whether to use deterministic or stochastic actions
-    :param render: (bool) Whether to render the environment or not
-    :param callback: (callable) callback function to do additional checks,
-        called after each step.
-    :param reward_threshold: (float) Minimum expected reward per episode,
-        this will raise an error if the performance is not met
-    :param return_episode_rewards: (bool) If True, a list of reward per episode
-        will be returned instead of the mean.
-    :return: (float, float) Mean reward per episode, std of reward per episode
-        returns ([float], [int]) when ``return_episode_rewards`` is True
-    """
-    # if isinstance(env, gym.Env):
-    #     assert env.num_envs == 1, "You must pass only one environment when using this function"
+def evaluate_policy(model, env, n_eval_episodes=10, render=False, return_episode_rewards=False):
 
     episode_rewards, episode_lengths = [], []
     for _ in range(n_eval_episodes):
         state = env.reset()
-        # done, state = False, None
-        done = False
-        episode_reward = 0.0
+        episode_reward = 0
         episode_length = 0
+        done = False
         while not done:
-            state = torch.FloatTensor(state).unsqueeze(0).to("cpu").detach()
-            action_dist, value = model.forward(state)
-            action = action_dist.sample().cpu()
-            # action, _, _ = model.policy(obs)
-            obs, reward, done, _info = env.step(torch.flatten(action).numpy())
-            # print(reward)
+            # 1. Select action according to policy
+            action, log_prob, value = model.policy(state)
+            # 2. Execute selected action
+            next_state, reward, done, _ = env.step(action.numpy()[0])
+            # 3. Integrate new experience into agent
+
+            action = action.detach()
+            log_prob = log_prob.detach()
+            state = torch.FloatTensor(state).unsqueeze(0).to(torch.device("cpu")).detach()
+            agent.memory.save(log_prob, value, state, action, reward, done)
+
+            state = next_state
             episode_reward += reward
-            if callback is not None:
-                callback(locals(), globals())
+        # print(nr_episode, ":", undiscounted_return)
             episode_length += 1
             if render:
                 env.render()
     #     print("--------------------")
-    #     episode_rewards.append(episode_reward)
-    #     episode_lengths.append(episode_length)
+        episode_rewards.append(episode_reward)
+        episode_lengths.append(episode_length)
     # print(episode_rewards)
     mean_reward = np.mean(episode_rewards)
     std_reward = np.std(episode_rewards)
-    if reward_threshold is not None:
-        assert mean_reward > reward_threshold, ('Mean reward below threshold: '
-                                                f'{mean_reward:.2f} < {reward_threshold:.2f}')
+
     if return_episode_rewards:
         return episode_rewards, episode_lengths
     return mean_reward, std_reward
+
 
 if __name__ == '__main__':
     # window_path = "../crawler_single/UnityEnvironment"
     linux_path = "../crawler_single/linux/dynamic_server/crawler_dynamic.x86_64"
     unity_env = UnityEnvironment(file_name=linux_path, seed=1, side_channels=[])
-    env = UnityToGymWrapper(unity_env=unity_env)
+    crawler_env = UnityToGymWrapper(unity_env=unity_env)
 
     params = {}
 
@@ -83,9 +62,9 @@ if __name__ == '__main__':
     # hidden_units: 2^x, bigger as input [256, 512]
     params["hidden_units"] = 512
 
-    params["nr_output_features"] = env.action_space.shape[0]
-    params["nr_input_features"] = env.observation_space.shape[0]
-    params["env"] = env
+    params["nr_output_features"] = crawler_env.action_space.shape[0]
+    params["nr_input_features"] = crawler_env.observation_space.shape[0]
+    params["env"] = crawler_env
 
     # Hyperparameters
     # min. two layer
@@ -104,5 +83,5 @@ if __name__ == '__main__':
 
     for i in range(160, 175):
         agent.ppo_net = torch.load("PPONet_{}".format(i))
-        mean_reward, std_reward = evaluate_policy(agent.ppo_net, env, n_eval_episodes=10, render=True)
+        mean_reward, std_reward = evaluate_policy(agent, crawler_env, n_eval_episodes=10, render=True)
         print("{}: {}, {}".format(i, mean_reward, std_reward))
